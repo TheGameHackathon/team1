6dc154268e71e83e2e1ee1b050cc0dcbee8bafb1
Ilya Zhdanov <zdravulin@gmail.com>
2022-07-04 11:24:37 +0500

    Add GameRepository methods

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Domain;
 
 namespace thegame.Services
@@ -12,19 +13,35 @@ namespace thegame.Services
 
     public class GamesRepository : IGamesRepository
     {
+        private readonly Dictionary<Guid, Game> entities = new Dictionary<Guid, Game>();
         public Game GetOrCreate(Guid id)
         {
-            throw new NotImplementedException();
+            if (entities.TryGetValue(id, out var game))
+            {
+                return Clone(id, game);
+            }
+
+            var entity = new Game(); //TODO
+            entities[id] = entity; //TODO
+            return Clone(id, entity);
         }
 
         public void Update(Game game)
         {
-            throw new NotImplementedException();
+            if (!entities.ContainsKey(game.Id))
+                return;
+
+            entities[game.Id] = Clone(game.Id, game);
         }
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            entities.Remove(id);
+        }
+
+        private Game Clone(Guid id, Game game)
+        {
+            return new Game(); // TODO
         }
     }
 }
