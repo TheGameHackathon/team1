5895b3cbd15573fd11e2fcdbabe85e49f963e6cc
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2022-07-04 13:39:06 +0500

    add random direction bot

---
+++ src/Domain/RandomBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    class RandomBot : IBot
+    {
+        public void MakeStep(Game currentGame)
+        {
+            var randomDirection = new Random().Next(37, 41);
+            currentGame.MoveCells(randomDirection);
+        }
+    }
+}
