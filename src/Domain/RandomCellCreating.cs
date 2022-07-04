fe830a8707c7370f4b2a2d81c098e20a19a82c0a
dedvnutree <tkachuk-p@mail.ru>
2022-07-04 11:25:27 +0500

    RandomCellCreating init2

--- src/Domain/RandomCellCreating.cs
+++ src/Domain/RandomCellCreating.cs
@@ -1,9 +1,14 @@
+using thegame.Models;
+
 namespace Domain;
 
 public class RandomCellCreating
 {
+    private CellDto[] cells;
     public Game CreateRandomCell(Game game)
     {
+        
+        
         return new Game();
     }
 }
