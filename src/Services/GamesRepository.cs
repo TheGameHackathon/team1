03d13c7bdac4239cfc6570f0536c738bea288a1c
Ilya Zhdanov <zdravulin@gmail.com>
2022-07-04 11:00:06 +0500

    Add GameRepository

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -1,7 +1,30 @@
+using System;
+using Domain;
+
 namespace thegame.Services
 {
-
-    public class GamesRepository
+    public interface IGamesRepository
     {
+        Game GetOrCreate(Guid id);
+        void Update(Game game);
+        void Delete(Guid id);
+    }
+
+    public class GamesRepository : IGamesRepository
+    {
+        public Game GetOrCreate(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
